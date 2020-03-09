7f7daf2a46b9c783894aafb194d2bdf839ea763d
Lukasz Falda <lukasz.falda@wearco.pl>
2020-03-09 13:56:38 +0100

    Rename base bundle class name

--- src/LuigisBoxBundle/DependencyInjection/AnswearLuigisBoxExtension.php
+++ src/LuigisBoxBundle/DependencyInjection/AnswearLuigisBoxExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class LuigisBoxExtension extends Extension
+class AnswearLuigisBoxExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
